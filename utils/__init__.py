a5afc13afec3a8dc177bd3a188a28a7346224ca4
Bryce Arden <bryce@centtech.com>
2017-10-01 12:37:29 -0500

    test pkg

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,21 +5,8 @@ __author__    = "Bryce Arden"
 __copyright__ = "Centaur Technologies"
 __date__      = "2017-03-03"
 
-from .io import *
-from .reg import *
-from .misc import *
-from .constants import *
-from .jinja2_ext import *
-
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
     __version__ = 'unknown'
-
-__all__ = (
-    io.__all__ +
-    reg.__all__ +
-    misc.__all__ +
-    jinja2_ext.__all__
-)
 
